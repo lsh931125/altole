83cbbe32335e60fe4ce58a1e428f8f526bb74eeb
임성호 <lsh931125@naver.com>
2021-03-13 21:10:47 +0900

    main

--- week8/naver_news.py
+++ week8/naver_news.py
@@ -1,18 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-naver_url = 'https://search.naver.com/search.naver?where=news&query=%EC%BD%94%EB%A1%9C%EB%82%98&sm=tab_srt&sort=1&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so%3Add%2Cp%3Aall%2Ca%3Aall&mynews=0&refresh_start=0&related=0'
-res = requests.get(naver_url)
+naverUrl = 'https://search.naver.com/search.naver?where=news&query=%EC%BD%94%EB%A1%9C%EB%82%98&sm=tab_srt&sort=0&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so%3Ar%2Cp%3Aall%2Ca%3Aall&mynews=0&refresh_start=0&related=0'
+res = requests.get(naverUrl)
 soup = BeautifulSoup(res.text, 'html.parser')
 listNews = soup.find_all('a', class_ = 'news_tit')
-print(listNews)
-# coronaNews = {}
-# num = 1
+# print(listNews)
 
 for i in listNews:
-    href = i.attrs['href']
+    link = i.attrs['href']
+    # print(link)
     title = i.attrs['title']
-    print(f'제목 = {title} \n 링크 = {href}')
-    # coronaNews[num] = '제목 = '+title+'링크 = '+href
-    # num += 1
-# print(coronaNews[1])
+    # print(title)
+    print(f'제목 = {title}\n링크 = {link}')
