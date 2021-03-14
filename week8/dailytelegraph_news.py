09e18790490e1e066e38e0408ccf2c1d60331acd
임성호 <lsh931125@naver.com>
2021-03-14 17:04:50 +0900

    main

--- week8/dailytelegraph_news.py
+++ week8/dailytelegraph_news.py
@@ -5,8 +5,21 @@
 # res = requests.get(daily_url)
 # print(res)
 
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome('./chromedriver')
-driver.get('https://www.dailytelegraph.com.au/search-results/?q=covid')
-print(driver.page_source)
+driver.get('https://www.dailytelegraph.com.au/search-results/?q=')
+# driver.execute_script("document.getElementsByClassName('ais-SearchBox-input search_box_input').value = 'corona'")
+driver.find_element_by_xpath('//*[@id="searchInput"]/div/form/input').click()
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="searchInput"]/div/form/input').send_keys('corona')
+time.sleep(1)
+# print(driver.page_source)
+# driver.find_element_by_xpath('//*[@id="searchInput"]/div/form/input').send_keys(Keys.ENTER)
+
+
+
+
